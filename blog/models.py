1b69f6e94f9f54cf983c78b4893cae6c5a1998f0
pazooki <mehrdad.pazooki@gmail.com>
2014-04-08 16:28:32 -0400

    blog user auth

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Author(models.Model):
-    user = models.OneToOneField(User)
+Author = User
 
 class Post(models.Model):
-    author = models.ForeignKey(User, related_name='posts')
+    author = models.ForeignKey(Author, related_name='posts')
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=255)
     body = models.TextField(blank=True, null=True)
