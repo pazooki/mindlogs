156e0bf8d18cfbd803669e8c4f793f21b41d3a8e
pazooki <mehrdad.pazooki@gmail.com>
2014-04-17 00:36:24 -0400

    post test_create_post PASSED

--- blog/tests.py
+++ blog/tests.py
@@ -12,9 +12,14 @@ class PostTests(APITestCase):
         self.client.login(username=Author.objects.get(username=mindlogger), password=mindlogger)
 
     def test_create_post(self):
+        test_fields = ['author', 'title', 'body']
         url = reverse('post-list')
         data = PostSerializer(Post.objects.get(pk=1)).data
         response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED, msg=response)
-        self.assertEqual(response.data, data, msg=(response.data, data))
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED, msg=(response, data))
+        self.assertEqual(
+            [response.data.get(field) for field in test_fields],
+            [data.get(field) for field in test_fields],
+            msg=(response.data, data)
+        )
 
