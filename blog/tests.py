19888f20bc14d3db2e977ccd3a0a320b6b5a330c
pazooki <mehrdad.pazooki@gmail.com>
2014-04-17 01:33:03 -0400

    PostTests better url handling

--- blog/tests.py
+++ blog/tests.py
@@ -13,13 +13,33 @@ class PostTests(APITestCase):
 
     def test_create_post(self):
         test_fields = ['author', 'title', 'body']
-        url = reverse('post-list')
-        data = PostSerializer(Post.objects.get(pk=1)).data
+        post_id = '1'
+        data = PostSerializer(Post.objects.get(pk=post_id)).data
+        url = reverse('post-detail')
         response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED, msg=(response, data))
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED, msg=(url, response.status_code, data))
         self.assertEqual(
             [response.data.get(field) for field in test_fields],
             [data.get(field) for field in test_fields],
             msg=(response.data, data)
         )
 
+    def test_get_post(self):
+        test_fields = ['author', 'title', 'body']
+        post_id = '1'
+        data = PostSerializer(Post.objects.get(pk=post_id)).data
+        url = reverse('post-detail', args=[post_id])
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg=(url, response.status_code, data))
+        self.assertEqual(
+            [response.data.get(field) for field in test_fields],
+            [data.get(field) for field in test_fields],
+            msg=(response.data, data)
+        )
+
+    def test_delete_post(self):
+        post_id = '1'
+        data = PostSerializer(Post.objects.get(pk=post_id)).data
+        url = reverse('post-detail', args=[post_id])
+        response = self.client.delete(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT, msg=(url, response.status_code, data))
